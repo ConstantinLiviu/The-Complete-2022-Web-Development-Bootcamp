6d1999bea79c6579fe3b433d772732dd1180d406
MaFomedanu <constantinliviudaniel@yahoo.com>
2022-10-14 01:05:11 +0300

    color palette, fonts and basic functionality of projects

---
+++ Section 5 - BasicJS/script.js
@@ -0,0 +1,8 @@
+const projEl = document.querySelector(".project-wrapper");
+const testEl = document.querySelector(".test");
+
+projEl.addEventListener("click", () => {
+  //   projEl.style.marginBottom = "20rem";
+  testEl.style.height = "20rem";
+  testEl.style.visibility = "visible";
+});
